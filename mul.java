759f39f00c1e31a16a9f6a2efed0491d4205ddc5
aryanniteshlankapalli <121013381+aryanniteshlankapalli@users.noreply.github.com>
2022-12-19 14:18:04 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Mul
+{
+static public void main(String[]afd)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter an integer");
+int n=s.nextInt();
+for(int i=1;i<=10;i++)
+{int m=n;
+System.out.println(n+"*"+i+"="+(n*i));}}}
